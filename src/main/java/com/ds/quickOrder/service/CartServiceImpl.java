5342dfefb9f604ae7abb93c78110d24d1ae43e2f
mjdilone <mjdilone7794@gmail.com>
2020-02-11 19:49:17 -0500

    ambi

--- src/main/java/com/ds/quickOrder/service/CartServiceImpl.java
+++ src/main/java/com/ds/quickOrder/service/CartServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import com.ds.quickOrder.controller.CartController;
@@ -17,6 +18,7 @@ public class CartServiceImpl implements CartService{
 	private static Logger log = LoggerFactory.getLogger(CartController.class);
 
 	@Autowired
+	@Lazy //possible fix for bean exception
 	CartDaoImpl cartDao;
 	
 	@Override
